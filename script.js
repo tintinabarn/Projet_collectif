a10f455d05f1c1dce2c49bd795c77bc39e0590b0
tintinabarn <90499363+tintinabarn@users.noreply.github.com>
2025-03-08 12:35:49 +0100

    Update script.js
    Removed temp server adress

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
 
         try {
-            const response = await fetch("https://startling-tanuki-efbcea.netlify.app/submit", {
+            const response = await fetch("/submit", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ name, text })
